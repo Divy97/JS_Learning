9f59c6e81946c77a917360cfb25efe96730e4b21
Divy <divyparekh1810@gmail.com>
2022-08-16 19:54:28 +0530

    imperative and declarative way of  a function

---
+++ functional_programming/impVSdec.js
@@ -0,0 +1,21 @@
+// is the square of a number is even
+
+//imperative way
+
+const x = 2;
+const xSquared = x * x;
+
+let isEven;
+
+if (xSquared % 2 == 0) {
+  isEven = true;
+} else {
+  isEven = false;
+}
+
+console.log(isEven);
+
+//Declarative way
+
+const isSquareEven = (x) => ((x * x) % 2 === 0 ? true : false);
+console.log(isSquareEven(4));
